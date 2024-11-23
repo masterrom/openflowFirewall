eed02e33885c17afc4ac6814c68c2c30e1c79822
Abdul <abdulwasay_mehar@live.com>
2024-11-23 16:28:28 -0500

    chore: optimized

--- L3FirewallTest2.py
+++ L3FirewallTest2.py
@@ -26,36 +26,7 @@ class Firewall (EventMixin):
 
     def __init__ (self,l2config,l3config):
         self.listenTo(core.openflow)
-        # Anti-spoofing Port Security.
-        # 
-        # Set SpoofingTable as a dict() which uses the source MAC address as key, and an array of the IP address and source
-        # switch port as value.
-        #
-        # For example:
-        #
-        #   SpoofingTable = {
-        #      # src MAC address     src IP          dst IP          src OVS port
-        #      "00:00:00:00:00:0a": ["192.168.2.10", "192.168.2.30", "1"],
-        #      "00:00:00:00:00:0b": ["192.168.2.20", "192.168.2.10", "2"],
-        #      "00:00:00:00:00:0c": ["192.168.2.30", "192.168.2.40", "3"]
-        #   }
-        #
-        # In this way, it is possible to identify if a MAC address had already used an IP, and block it as required by the
-        # Lab's algorithm.
-        #
-        # To implement the "Bonus Points" and also address MAC address spoofing, it is necessary to traverse the Dict
-        # through the values until the source port is found again.  When we see that the same port ha originated different
-        # MAC addresses, it means they have been spoofed (or the user has attached a cascading switch, which is practically
-        # the same and a target for Port Security to block).
-        # 
-        # The same mechanism can be found to identify users that have spoofed _both_ IP addresses and MAC addresses, which
-        # would be the most advanced mitigation possible for this lab.
-        #
-        # The dict "BlockedTable" is identical to SpoofingTable (except it doesn't stop a switch port) but represents
-        # combinations of attackers that have already been blocked and the flow has been saved to CSV - this is to avoid
-        # writing them multiple times in the l3firewall file.
-        # type: ignore
-
+        
         self.patternTable = dict ()    # Corrispondence of MAC addresses with IPs and ports
         self.currentlyBlocked = dict ()     # type: ignore # Table of blocked attacks
         self.disabled_Mac_pair = []
@@ -125,15 +96,12 @@ class Firewall (EventMixin):
         event.connection.send(msg)
 
     def allowOther(self, event, action=None):
-        log.debug ("Execute allowOther")
         msg = of.ofp_flow_mod()
         match = of.ofp_match()
-        #action = of.ofp_action_output(port = of.OFPP_NORMAL)
         msg.actions.append(action)
         event.connection.send(msg)
 
     def installFlow(self, event, offset, srcmac, dstmac, srcip, dstip, sport, dport, nwproto):
-        log.debug ("Execute installFlow")
         msg = of.ofp_flow_mod()
         match = of.ofp_match()
         if(srcip != None):
@@ -159,12 +127,9 @@ class Firewall (EventMixin):
         event.connection.send(msg)
 
     def replyToIP(self, packet, match, event):
-        log.debug ("Execute replyToIP")
+       
         srcmac = str(match.dl_src)
         dstmac = str(match.dl_src)
-        # sport = str(match.tp_src)
-        # dport = str(match.tp_dst)
-        # nwproto = str(match.nw_proto)
 
         with open(l3config) as csvfile:
             log.debug("Reading log file !")
@@ -212,11 +177,11 @@ class Firewall (EventMixin):
         '''
         self.connection = event.connection
 
-        for spoofmac, spoofvalues in self.patternTable.items():
+        for hostMac, pattern in self.patternTable.items():
 
-            srcmac = spoofmac
-            srcip = spoofvalues[0]
-            dstip = spoofvalues[1]
+            srcmac = hostMac
+            srcip = pattern[0]
+            dstip = pattern[1]
             log.debug ('Loading blocked flows: srcmac=%s, srcip=%s, dstip=%s' %
                     (str(srcmac), str(srcip), str(dstip)))
             #print source,destination
@@ -242,121 +207,6 @@ class Firewall (EventMixin):
 
             log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
-    def addRuleToCSV(self, srcmac='any', srcip='any', dstip='any'):
-        
-        log.debug("Entered addRuleToCSV")
-
-        # Check if the rule is not already saved
-        # If not, add to firewall rules in memory and then in the CSV file
-
-        to_add = True
-        for spoofmac, spoofvalues in self.currentlyBlocked.items():
-            if spoofmac == str(srcmac) and spoofvalues[0] == str(srcip) and spoofvalues[1] == str(dstip):
-                log.debug("No need to write log file - entry already present")
-                to_add = False
-                break
-
-        if to_add: 
-            self.currentlyBlocked [str(srcmac)] = [str(srcip), str(dstip)]
-            # Open in append mode
-            with open(l3config, 'a') as csvfile:
-                log.debug("Writing log file !")
-
-                csvwriter = csv.DictWriter(csvfile, fieldnames=[
-                    'priority','src_mac','dst_mac','src_ip','dst_ip','src_port','dst_port','nw_proto',])
-                log.debug("Saving individual rule parameters in rule dict !")
-                csvwriter.writerow({
-                    'priority': 32768,
-                    'src_mac' : str(srcmac),
-                    'dst_mac' : 'any',
-                    'src_ip'  : str(srcip),
-                    'dst_ip'  : str(dstip),
-                    'src_port': 'any',
-                    'dst_port': 'any',
-                    'nw_proto': 'any',
-                    })
-                log.debug("Saved: srcip=%s dstip=%s srcmac=%s" % (str(srcip), str(dstip), str(srcmac)))
-
-    def verifyPortSecurity(self, packet, match=None, event=None):
-
-        log.debug("Into verifyPortSecurity")
-
-        srcmac = None
-        srcip = None
-        dstip = None
-
-        if packet.type == packet.IP_TYPE:
-            ip_packet = packet.payload
-            if ip_packet.srcip == None or ip_packet.dstip == None:
-                log.debug("Packet meaningless for Port Security (likely IPv6)")
-                return True
-            if packet.src not in self.patternTable:        # MAC address is not in the spoofing table. Checking IP address
-                for spoofmac, spoofvalues in self.patternTable.items():
-                    # IP already present with another MAC address: MAC spoofing!
-                    # This is the most "advanced" case (Bonus Point) for this Lab.
-                    if str(spoofvalues[0]) == str(ip_packet.srcip):
-                        log.debug("*** MAC spoofing attempt! IP %s already present for MAC %s and port %s, Requested: from %s on port %s ***" %
-                            (str(ip_packet.srcip), str(spoofmac), str(spoofvalues[1]), str(packet.src), str(event.port)))
-                        #self.addRuleToCSV (str(spoofmac), str(ip_packet.srcip))
-                        # Block the source/destination IP address for any MAC, to protect the victim
-                        srcmac = None
-                        srcip = str(ip_packet.srcip)
-                        dstip = str(ip_packet.dstip)
-                        self.addRuleToCSV ('any', srcip, dstip)
-                        #return False
-                # The flow is a new legitimate one. Adding it to the table and allowing the packet.
-                self.patternTable [packet.src] = [ip_packet.srcip, ip_packet.dstip, event.port]
-                log.debug("Adding Port Security entry: %s, %s, %s, %s" %
-                    (str(packet.src), str(ip_packet.srcip), str(ip_packet.dstip), str(event.port)))
-                return True
-            else:                                           # MAC address is already in the spoofing table. Checking the cases.
-                # The identical entry is present in the table. This probably means, OVS has expired the flow and it is representing.
-                # In this case the flow is okay.
-                if self.patternTable.get(packet.src) == [ip_packet.srcip, ip_packet.dstip, event.port]:
-                    log.debug("Port Security entry already present: %s, %s, %s, %s" %
-                        (str(packet.src), str(ip_packet.srcip), str(ip_packet.dstip), str(event.port)))
-                    return True
-                else:
-                    # Port Security Check
-                    oldIp = self.patternTable.get(packet.src)[0]
-                    oldPort = self.patternTable.get(packet.src)[1]
-                    
-                    if oldIp != ip_packet.srcip:
-                        log.debug("----- Spoofing attempt detected. srcMac= %s, packetSrcIP= %s, oldSrcIP= %s", str(packet.src), str(ip_packet.srcip), str(oldIp))
-                        # Block the MAC address
-                        srcmac = str(packet.src)
-                        srcip = None
-                        dstip = str(ip_packet.dstip)
-                        self.addRuleToCSV (srcmac, 'any', dstip)
-                        
-                    # Second: flow has been seen on a different port. This is likely a routing or spanning tree problem,
-                    # more hardly an attack. Without better knowledge of the topology, we need to allow the flow for this lab.
-                    if oldPort != event.port:
-                        log.debug("*** Port has changed for the same MAC address: new port %s, MAC %s: it was IP %s on port %s], Requested: %s ***" %
-                            (str(oldPort), str(packet.src), str(ip_packet.srcip), str(event.port), str(ip_packet.dstip)))
-                        return True
-
-                    log.debug("You should never get here. If you do, I did something wrong!")
-
-                    # Future extension: count refused packet at the switch port level, and evaluate a threshold.
-                    # Over the threshold, block the port altogether to save the rest of the environment
-                    return True
-
-        if packet.type == packet.ARP_TYPE:
-            log.debug("ARP security - for future extension")
-            return True
-
-        srcmac = srcmac
-        dstmac = None
-        sport = None
-        dport = None
-        nwproto = str(match.nw_proto)
-
-        log.debug("verifyPortSecurity - installFlow")
-        self.installFlow(event, 32768, srcmac, None, srcip, dstip, None, None, nw_proto)
-
-        return False
-
     def writeToFirewall(self, srcMac='any', srcIP='any', dstIP='any'):
         
         log.debug("----- Adding new Firewall Rule")
@@ -364,33 +214,33 @@ class Firewall (EventMixin):
         # Check if the rule is not already saved
         # If not, add to firewall rules in memory and then in the CSV file
 
-        addRule = True
-        for attackerMac, pattern in self.currentlyBlocked.items():
-            if attackerMac == str(srcMac) and pattern[0] == str(srcIP) and pattern[1] == str(dstIP):
-                log.debug('Rule already exists...')
-                addRule = False
-                break
+        # addRule = True
+        # for attackerMac, pattern in self.currentlyBlocked.items():
+        #     if attackerMac == str(srcMac) and pattern[0] == str(srcIP) and pattern[1] == str(dstIP):
+        #         log.debug('Rule already exists...')
+        #         addRule = False
+        #         break
 
-        if addRule: 
-            log.debug("----- Saving: srcIP=%s dstIP=%s srcMAC=%s" % (str(srcIP), str(dstIP), str(srcMac)))
-            log.debug('----- Storeing new rule into memory...')
+        # if addRule: 
+        log.debug("----- Saving: srcIP=%s dstIP=%s srcMAC=%s" % (str(srcIP), str(dstIP), str(srcMac)))
+        log.debug('----- Storeing new rule into memory...')
 
-            self.currentlyBlocked [str(srcMac)] = [str(srcIP), str(dstIP)]
-            with open(l3config, 'a') as csvfile:
-                log.debug("Writing log file !")
+        self.currentlyBlocked [str(srcMac)] = [str(srcIP), str(dstIP)]
+        with open(l3config, 'a') as csvfile:
+            log.debug("Writing log file !")
 
-                csvwriter = csv.DictWriter(csvfile, fieldnames=[
-                    'priority','src_mac','dst_mac','src_ip','dst_ip','src_port','dst_port','nw_proto',])
-                csvwriter.writerow({
-                    'priority': 32768,
-                    'src_mac' : str(srcMac),
-                    'dst_mac' : 'any',
-                    'src_ip'  : str(srcIP),
-                    'dst_ip'  : str(dstIP),
-                    'src_port': 'any',
-                    'dst_port': 'any',
-                    'nw_proto': 'any',
-                    })
+            csvwriter = csv.DictWriter(csvfile, fieldnames=[
+                'priority','src_mac','dst_mac','src_ip','dst_ip','src_port','dst_port','nw_proto',])
+            csvwriter.writerow({
+                'priority': 32768,
+                'src_mac' : str(srcMac),
+                'dst_mac' : 'any',
+                'src_ip'  : str(srcIP),
+                'dst_ip'  : str(dstIP),
+                'src_port': 'any',
+                'dst_port': 'any',
+                'nw_proto': 'any',
+                })
 
     def portSecurity(self, packet, match=None, event=None):
 
@@ -422,7 +272,16 @@ class Firewall (EventMixin):
                         srcmac = str(packet.src)
                         srcip = None
                         dstip = str(ip_packet.dstip)
-                        self.writeToFirewall (srcmac, 'any', dstip)
+
+                        rulePreExists = True
+                        for attackerMac, pattern in self.currentlyBlocked.items():
+                            if attackerMac == str(srcmac) and pattern[0] == 'any' and pattern[1] == str(dstip):
+                                log.debug('Rule already exists...')
+                                rulePreExists = False
+                                break
+                        
+                        if rulePreExists:
+                            self.writeToFirewall (srcmac, 'any', dstip)
                         
                     return True
             else:
